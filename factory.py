2cb38c15565a52e070d458bf451b9f1224f694be
mullender <matthijs.mullender@gmail.com>
2026-08-09 16:48:26 -0700

    chore: release version 0.3.2.0

--- factory.py
+++ factory.py
@@ -24,7 +24,7 @@ from typing import Any, Callable
 
 SOURCE_ROOT = Path(__file__).resolve().parent
 TEMPLATE_ROOT = SOURCE_ROOT / "templates" / "project" / ".factory"
-VERSION = "0.3.1.0"
+VERSION = "0.3.2.0"
 TEMPLATE_STATE_FORMAT = 1
 MANAGED_TEMPLATE_PATTERNS = (
     "config/*.md",
